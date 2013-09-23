08f421527a13db5f442f51eb6abfa9ec7d34e3e0
Eli Duke <elimduke@gmail.com>
2013-09-22 21:04:44 -0700

    Streamlined the script after learning a few things from poopypants.net

--- README.md
+++ README.md
@@ -1,26 +1,25 @@
 Once upon a time...
 
-On September 25th, 2008, I was visiting my brother and sister-in-law in Portland, Oregon, and I snapped this photo:
+On September 25th, 2008, while visiting my brother and sister-in-law in Portland, Oregon, I snapped this photo:
 
-Professor Poopypants's Pocket-Sized "Name Change-O-Chart 2000"
-[http://flickr.com/elisfanclub/2908192137]()
+[http://flickr.com/elisfanclub/2908192137](Professor Poopypants's Pocket-Sized "Name Change-O-Chart 2000")
 
 It was taped to the wall in their bathroom, right by the mirror over the sink. I didn't think much of it at the time, but Professor Poopypants sure did make us laugh. Oh, the laughs!
 
-According to Professor Poopypants, my new name is Crusty Bubble Tush. Hilarious.
+According to Professor Poopypants, my new name is Crusty Bubbletush. Hilarious.
 
-Fast-forward almost three and a half years, and that little innocent photo is my 6th-most-viewed photo on Flickr. I mean, it's only got 2,660 views (as of January 30th, 2012), which isn't much in the grand scheme of the internets, but still, people just can't get enough of Professor Poopypants and his wacky names!
+Fast-forward about 5 years and that innocent little photo is my most popular photo on Flickr! I mean, it's only got about 13,000 views (as of September 22nd, 2013), which isn't much in the grand scheme of the internets, but still, people just can't get enough of Professor Poopypants and his wacky names!
 
-On a whim tonight, I decided to scriptify the "Name Change-O-Chart 2000," as my first-ever GitHub Repo. Hopefully this sets the tone for all my future activity here on the githubs.
+On a whim back in January 2012, I decided to scriptify the "Name Change-O-Chart 2000," as my first-ever GitHub Repo. Hopefully this sets the tone for all my future activity here on the githubs.
 
-Feel free to download poopypants.rb and give a go on your machine. From the same directory as that file, run:
+Feel free to download poopypants.rb and give it a go on your machine. From the same directory as that file, run:
 
     ruby poopypants.rb
 
 You can see the "official" Professor Poopypants website here:
 
-[http://apps.scholastic.com/captainunderpants/NameGame/play.htm]()
+[http://www.scholastic.com/captainunderpants/poopypants.htm]()
 
 Looks like Professor Poopypants is in cahoots with Captain Underpants!
 
-P.S. I just typed "poopypants" 9 times. FTW!
+P.S. I just typed "poopypants" 10 times. FTW!
--- poopypants.rb
+++ poopypants.rb
@@ -1,31 +1,29 @@
 ### PROFESSOR POOPYPANTS NAME CHANGE-O-RUBY-SCRIPT 2000 ###
 
-first_firsts = [["A", "Stinky"], ["B", "Lumpy"], ["C", "Buttercup"], ["D", "Gidget"], ["E", "Crusty"], ["F", "Greasy"], ["G", "Fluffy"], ["H", "Cheeseball"], ["I", "Chim-Chim"], ["J", "Poopsie"], ["K", "Flunky"], ["L", "Booger"], ["M", "Pinky"], ["N", "Zippy"], ["O", "Goober"], ["P", "Doofus"], ["Q", "Slimy"], ["R", "Loopy"], ["S", "Snotty"], ["T", "Falafel"], ["U", "Dorky"], ["V", "Squeezit"], ["W", "Oprah"], ["X", "Skipper"], ["Y", "Dinky"], ["Z", "Zsa-Zsa"]]
-
-first_lasts  = [["A", "Diaper"], ["B", "Toilet"], ["C", "Giggle"], ["D", "Bubble"], ["E", "Girdle"], ["F", "Barf"], ["G", "Lizard"], ["H", "Waffle"], ["I", "Cootie"], ["J", "Monkey"], ["K", "Potty"], ["L", "Liver"], ["M", "Banana"], ["N", "Rhino"], ["O", "Burger"], ["P", "Hamster"], ["Q", "Toad"], ["R", "Gizzard"], ["S", "Pizza"], ["T", "Gerbil"], ["U", "Chicken"], ["V", "Pickle"], ["W", "Chuckle"], ["X", "Tofu"], ["Y", "Gorilla"], ["Z", "Stinker"]]
-
-last_lasts   = [["A", "Head"], ["B", "Mouth"], ["C", "Face"], ["D", "Nose"], ["E", "Tush"], ["F", "Breath"], ["G", "Pants"], ["H", "Shorts"], ["I", "Lips"], ["J", "Honker"], ["K", "Butt"], ["L", "Brain"], ["M", "Tushie"], ["N", "Chunks"], ["O", "Hiney"], ["P", "Biscuits"], ["Q", "Toes"], ["R", "Buns"], ["S", "Fanny"], ["T", "Sniffer"], ["U", "Sprinkles"], ["V", "Kisser"], ["W", "Squirt"], ["X", "Humperdinck"], ["Y", "Brains"], ["Z", "Juice"]]
+firsts  = [["A", "Stinky"], ["B", "Lumpy"], ["C", "Buttercup"], ["D", "Gidget"], ["E", "Crusty"], ["F", "Greasy"], ["G", "Fluffy"], ["H", "Cheeseball"], ["I", "Chim-Chim"], ["J", "Poopsie"], ["K", "Flunky"], ["L", "Booger"], ["M", "Pinky"], ["N", "Zippy"], ["O", "Goober"], ["P", "Doofus"], ["Q", "Slimy"], ["R", "Loopy"], ["S", "Snotty"], ["T", "Falafel"], ["U", "Dorky"], ["V", "Squeezit"], ["W", "Oprah"], ["X", "Skipper"], ["Y", "Dinky"], ["Z", "Zsa-Zsa"]]
+middles = [["A", "Diaper"], ["B", "Toilet"], ["C", "Giggle"], ["D", "Bubble"], ["E", "Girdle"], ["F", "Barf"], ["G", "Lizard"], ["H", "Waffle"], ["I", "Cootie"], ["J", "Monkey"], ["K", "Potty"], ["L", "Liver"], ["M", "Banana"], ["N", "Rhino"], ["O", "Burger"], ["P", "Hamster"], ["Q", "Toad"], ["R", "Gizzard"], ["S", "Pizza"], ["T", "Gerbil"], ["U", "Chicken"], ["V", "Pickle"], ["W", "Chuckle"], ["X", "Tofu"], ["Y", "Gorilla"], ["Z", "Stinker"]]
+lasts   = [["A", "head"], ["B", "mouth"], ["C", "face"], ["D", "nose"], ["E", "tush"], ["F", "breath"], ["G", "pants"], ["H", "shorts"], ["I", "lips"], ["J", "honker"], ["K", "butt"], ["L", "brain"], ["M", "tushie"], ["N", "chunks"], ["O", "hiney"], ["P", "biscuits"], ["Q", "toes"], ["R", "buns"], ["S", "fanny"], ["T", "sniffer"], ["U", "sprinkles"], ["V", "kisser"], ["W", "squirt"], ["X", "humperdinck"], ["Y", "brains"], ["Z", "juice"]]
 
 ###
 
 first_name = ""
 while first_name == ""
-  print "What is your FIRST name? "
-  first_name = gets.chomp
+  print "Please type your FIRST name and hit enter: "
+  first_name = gets.chomp.gsub(/\W+|\d+|_/, '')
 end
 
 last_name = ""
 while last_name == ""
-  print "What is your LAST name? "
-  last_name = gets.chomp
+  print "Now type your LAST name and hit enter: "
+  last_name = gets.chomp.gsub(/\W+|\d+|_/, '')
 end
 
-first_first = first_name.gsub(/\W+|\d+/, '')[0].chr.capitalize
-first_last  = last_name.gsub(/\W+|\d+/, '')[0].chr.capitalize
-last_last   = last_name.gsub(/\W+|\d+/, '')[-1].chr.capitalize
+first   = first_name[0].chr.capitalize
+middle  = last_name[0].chr.capitalize
+last    = last_name[-1].chr.capitalize
 
-new_first_first = first_firsts.select{|f| f[0] == first_first}
-new_first_last  = first_lasts.select{|f| f[0]  == first_last}
-new_last_last   = last_lasts.select{|f| f[0]   == last_last}
+new_first   = firsts.select{|f| f[0] == first}[0][1]
+new_middle  = middles.select{|f| f[0] == middle}[0][1]
+new_last    = lasts.select{|f| f[0] == last}[0][1]
 
-puts "According to Professor Poopypants, your new name is #{new_first_first[0][1]} #{new_first_last[0][1]} #{new_last_last[0][1]}."
+puts "Hey there, #{first_name.capitalize} #{last_name.capitalize}! According to Professor Poopypants, your new name is #{new_first} #{new_middle}#{new_last}. Haha! :)"
